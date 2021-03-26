71580f649989903cbff553dc6a18df174a5c2176
Breanna Starika <starikabreanna@gmail.com>
2021-03-26 11:48:51 -0600

    Updated 3/26

--- myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -8,8 +8,12 @@ namespace myConsoleProject.cs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
         }
     }
 }
