6d8afe0f559ed942db372aec9d39ab8c16ef9d16
Breanna Starika <starikabreanna@gmail.com>
2021-04-05 15:29:32 -0600

    Updated 4/5

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)                 //main() method
+        {
+
+            mathoperations m = new mathoperations();    //create object to instantiate the class and call on methods
+            int answer = m.mathOp(20);                  //addition method
+            Console.WriteLine(answer);
+
+            int answer2 = m.mathOp(2.2);                //decimal and subtraction method
+            Console.WriteLine(answer2);
+
+            int answer3 = m.mathOp("50");               //string and multiplication method
+            Console.WriteLine(answer3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethod/MainMethod/mathoperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class mathoperations //created class
+    {
+
+        public int mathOp(int num)                   //method mathOp()
+        {   
+            int result = num + num;                  //takes integer and does addition 
+            return result;                           //returns answer as integer 
+       
+        }
+        public int mathOp(double dec)                //method mathOp()
+        {
+            int result = Convert.ToInt32(dec - dec); //takes decimal and does subtraction
+            return result;                           //returns answer as integer
+        }
+
+        public int mathOp(string strNum)            //method mathOp()
+        {
+            int num = Convert.ToInt32(strNum);      //takes string and conversts to int
+            int result = num * num;                 //does multiplication
+            return result;                          //returns answer as integer
+        }
+    }
+}
