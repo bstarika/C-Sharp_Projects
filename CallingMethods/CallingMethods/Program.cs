8cd974dbb91344ba8d2d96008bf9e9365b9c973d
Breanna Starika <starikabreanna@gmail.com>
2021-04-05 11:29:39 -0600

    Updated 4/5

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            mathoperation m = new mathoperation(); //created object to call methods in mathoperation class
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //user input
+
+            m.Add(userInput); //calling method Add
+            m.Subtract(userInput); //calling method Subtract
+            m.Multiply(userInput); //caling method Multiply
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ CallingMethods/CallingMethods/mathoperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class mathoperation  //class mathoperation
+    {
+
+        public void Add(int num)   //method Add
+        {
+            int result = num + num;
+            Console.WriteLine(result);
+        }
+
+        public void Subtract(int num)   //method Subtract
+        {
+            int result = num - num;
+            Console.WriteLine(result);
+        }
+
+        public void Multiply(int num)   //method Multiply
+        {
+            int result = num * num;
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+
