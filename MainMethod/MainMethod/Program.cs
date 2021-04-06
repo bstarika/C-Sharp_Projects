4b3df79cac8324a0f2a38c0c2bee9916c59b3cde
Breanna Starika <starikabreanna@gmail.com>
2021-04-05 18:10:39 -0600

    Updated 4/5

--- MainMethod/MainMethod/Program.cs
+++ MainMethod/MainMethod/Program.cs
@@ -12,15 +12,36 @@ namespace MainMethod
         {
 
             mathoperations m = new mathoperations();    //create object to instantiate the class and call on methods
-            int answer = m.mathOp(20);                  //addition method
-            Console.WriteLine(answer);
 
-            int answer2 = m.mathOp(2.2);                //decimal and subtraction method
-            Console.WriteLine(answer2);
 
-            int answer3 = m.mathOp("50");               //string and multiplication method
-            Console.WriteLine(answer3);
+            Console.WriteLine("Please enter in your first number."); 
+            int userInput1 = Convert.ToInt32(Console.ReadLine());   //user enters number
+
+            Console.WriteLine("What is your second number? This number is optional. To bypass enter in no"); //second number optional
+            string userInput2 = Console.ReadLine();
+
+            if(userInput2 == "no")  //if user types no, method used will return whatever number they entered first + 10
+            {
+                Console.WriteLine(m.Add(userInput1));
+            }
+
+            else
+            {
+                int userInputMod = Convert.ToInt32(userInput2); //if user enters in number, it will be added to the first
+                Console.WriteLine(m.Add(userInput1, userInputMod));
+            }
+
             Console.ReadLine();
+            //mathoperations m = new mathoperations();    //create object to instantiate the class and call on methods
+            //int answer = m.mathOp(20);                  //addition method
+            //Console.WriteLine(answer);
+
+            //int answer2 = m.mathOp(2.2);                //decimal and subtraction method
+            //Console.WriteLine(answer2);
+
+            //int answer3 = m.mathOp("50");               //string and multiplication method
+            //Console.WriteLine(answer3);
+            //Console.ReadLine();
         }
     }
 }
--- MainMethod/MainMethod/mathoperations.cs
+++ MainMethod/MainMethod/mathoperations.cs
@@ -9,23 +9,29 @@ namespace MainMethod
     class mathoperations //created class
     {
 
-        public int mathOp(int num)                   //method mathOp()
-        {   
-            int result = num + num;                  //takes integer and does addition 
-            return result;                           //returns answer as integer 
+        public int Add(int num1, int num2 = 10) //two parameters, one optional
+        {
+            int result = num1 + num2;
+            return result;                      //returns integer
+        }
+        
+        //public int mathOp(int num)                   //method mathOp()
+        //{   
+        //    int result = num + num;                  //takes integer and does addition 
+        //    return result;                           //returns answer as integer 
        
-        }
-        public int mathOp(double dec)                //method mathOp()
-        {
-            int result = Convert.ToInt32(dec - dec); //takes decimal and does subtraction
-            return result;                           //returns answer as integer
-        }
+        //}
+        //public int mathOp(double dec)                //method mathOp()
+        //{
+        //    int result = Convert.ToInt32(dec - dec); //takes decimal and does subtraction
+        //    return result;                           //returns answer as integer
+        //}
 
-        public int mathOp(string strNum)            //method mathOp()
-        {
-            int num = Convert.ToInt32(strNum);      //takes string and conversts to int
-            int result = num * num;                 //does multiplication
-            return result;                          //returns answer as integer
-        }
+        //public int mathOp(string strNum)            //method mathOp()
+        //{
+        //    int num = Convert.ToInt32(strNum);      //takes string and conversts to int
+        //    int result = num * num;                 //does multiplication
+        //    return result;                          //returns answer as integer
+        //}
     }
 }
