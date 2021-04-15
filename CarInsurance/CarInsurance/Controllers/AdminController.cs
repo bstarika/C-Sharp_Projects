f1bea25ec6fe4fb82c1b8ce35d271d4bacf19d96
Breanna Starika <starikabreanna@gmail.com>
2021-04-15 16:12:59 -0600

    Updated 4/15

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Admin()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insuree = db.Insurees;
+                var insureeList = new List<Insuree>();
+
+                foreach(var i in insuree)
+                {
+                    var insPerson = new Insuree();
+                    insPerson.FirstName = i.FirstName;
+                    insPerson.LastName = i.LastName;
+                    insPerson.EmailAddress = i.EmailAddress;
+                    insPerson.Quote = i.Quote;
+
+                    insureeList.Add(insPerson);
+              
+                }
+                return View(insureeList);
+            }
+            
+        }
+    }
+}
